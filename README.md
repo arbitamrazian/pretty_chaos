de70493fd9e9749c9a88ec73866582944570eae3
Arbi Tamrazian <arbi.tamrazian@dreamhost.com>
2017-05-31 15:17:56 -0700

    add code and image

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pretty_chaos
+R Code to generate the Sierpinski triangle.  Just for fun!
+![Image of t-values](http://i.imgur.com/h7du5Ac.png)
---
+++ chaos_triangle.R
@@ -0,0 +1,18 @@
+nmonte = 10000
+
+lookup.x = c(0,0.5,1)
+lookup.y = c(1,0,1)
+plot(lookup.x,lookup.y)
+curr_x = 0.5
+curr_y = 0.7
+for (i in 1:nmonte ){
+  points(curr_x,curr_y,pch=20)
+  random = ceiling(3*runif(1))
+  next_x = lookup.x[random]
+  next_y = lookup.y[random]
+  prev_x = curr_x
+  prev_y = curr_y
+  curr_x = (prev_x + next_x)/2
+  curr_y = (prev_y + next_y)/2
+
+}
